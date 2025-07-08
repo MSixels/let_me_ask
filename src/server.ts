7c9a07dea503007c4a97fa488bf8bf5fc8b7be67
Matheus Sixel <msixel22@gmail.com>
2025-07-08 12:15:39 -0300

    criando rota no back para consumir listagem de salas

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
 import { fastify } from 'fastify'
-import { sql } from './db/connection.ts'
 import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from 'fastify-type-provider-zod'
 import { fastifyCors } from '@fastify/cors'
 import { env } from './env.ts'
+import { getRoomsRoute } from './http/routes/get-rooms.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -17,7 +17,7 @@ app.get ('/health', () => {
   return 'OK'
 })
 
-app.listen({ port: env.PORT }).then(() => {
+app.register(getRoomsRoute)
 
-})
+app.listen({ port: env.PORT })
 
